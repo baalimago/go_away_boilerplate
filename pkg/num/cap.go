fbed6c0ed723b7f74d50686de073a4d9f5a722c4
imago <l.kinde94@outlook.com>
2024-01-27 22:31:41 +0200

    Made cap <= and >=

--- pkg/num/cap.go
+++ pkg/num/cap.go
@@ -6,12 +6,13 @@ type Number interface {
 	constraints.Integer | constraints.Float
 }
 
+// Cap n so that it's more than or equal to min, and less than or equal to max
 func Cap[N Number](n, min, max N) (ret N) {
-	if n < min {
+	if n <= min {
 		return min
 	}
 
-	if n > max {
+	if n >= max {
 		return max
 	}
 
