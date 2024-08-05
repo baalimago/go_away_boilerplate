f0a376217202f73f2e5fc0c90bade74342054ab9
imago <l.kinde94@outlook.com>
2024-08-05 19:43:18 +0300

    Added MonitorV2

--- pkg/shutdown/shutdown.go
+++ pkg/shutdown/shutdown.go
@@ -32,3 +32,29 @@ func Monitor(cancel context.CancelFunc) {
 		}
 	}
 }
+
+// MonitorV2 is the same as Monitor except for two points:
+// 1. It breaks on ctx cancel
+// 2. It doesn't append newline on the warns or error prints
+func MonitorV2(ctx context.Context, cancel context.CancelFunc) {
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
+	amountOfCancels := 0
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-signalCh:
+			if amountOfCancels == 0 {
+				ancli.PrintWarn("initiated forceful shutdown")
+				cancel()
+			} else if amountOfCancels == 1 {
+				ancli.PrintWarn("graceful shutdown ongoing, cancel again to force shutdown")
+			} else {
+				ancli.PrintErr("forcing shutdown")
+				os.Exit(1)
+			}
+			amountOfCancels++
+		}
+	}
+}
