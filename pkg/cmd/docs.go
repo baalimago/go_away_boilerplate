45d97a360763d700311e583e5d3ffbce68c4af0b
imago <l.kinde94@outlook.com>
2026-01-02 09:22:14 +0200

    feat: Add cmd package
    This is extracted from wd-41 as I'm using it in multiple places now

---
+++ pkg/cmd/docs.go
@@ -0,0 +1,3 @@
+// Package cmd is yet nother cli tool abstraction. It's intended to be simple and
+// pragmatic. See tests for example usecase.
+package cmd
---
+++ pkg/cmd/version/docs.go
@@ -0,0 +1,3 @@
+// Package version is a util which finds the version and prints it
+// It's intended to be used with the go_away_boilerplate/pkg/cmd package
+package version
